5f88b0356ce38f4b06f7a3fb0f6bca01ffc6cfed
marc.fauser@fauser-ag.com <marc.fauser@fauser-ag.com@615d7cb5-8244-55fa-4de2-d9adc1fea6c0>
2011-08-02 19:12:04 +0000

    added logging added incremental backups

--- trunk/backup_hMailServer.cmd
+++ trunk/backup_hMailServer.cmd
@@ -2,14 +2,31 @@
 ::On Vista, the path is system32 when you start the script as an administrator
 cd /D "%~dp0"
 
-::Only needed with integrated MSSQL / MYSQL Database
-@echo Stopping hMailServer
-net stop hMailServer
+setlocal
 
-diskshadow -s diskshadow.script
+set CURRENT_DATE=%DATE%
+set PRETTY_DATE=%CURRENT_DATE:~-4%-%CURRENT_DATE:~3,2%-%CURRENT_DATE:~0,2%
+::Pretty date format yyyy-mm-dd. Works for date format dd.mm.yyyy
+
+set TMPVBSWD=%TEMP%\_TMP$.VBS
+echo WScript.Echo WeekDay( Date, vbMonday ) >%TMPVBSWD%
+for /F %%? in ('CSCRIPT //NOLOGO %TMPVBSWD%') do set WEEKDAY=%%?
+del %TMPVBSWD%
+echo WScript.Echo WeekDayName( WeekDay( Date, vbMonday ), False, vbMonday ) >%TMPVBSWD%
+for /F %%? in ('CSCRIPT //NOLOGO %TMPVBSWD%') do set WEEKDAYNAME=%%?
+del %TMPVBSWD%
+
+set LOGFILE="c:\Backup\hmailserver_backup-%PRETTY_DATE%.log"
+::Only needed with integrated MSSQL / MYSQL Database
+@echo Stopping hMailServer>%LOGFILE%
+net stop hMailServer>>%LOGFILE%
+
+diskshadow -s diskshadow.script>>%LOGFILE%
 
 ::In case something goes wrong in the script
-@echo Starting hMailServer
-net start hMailServer
+@echo Starting hMailServer>>%LOGFILE%
+net start hMailServer>>%LOGFILE%
 
-pause
+endlocal
+
+:::pause
